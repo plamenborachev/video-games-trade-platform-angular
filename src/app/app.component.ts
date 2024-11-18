3e63a422998ad8a9f8d45a3372d885d7f299535a
Plamen Borachev <plamen.borachev@gmail.com>
2024-11-18 16:46:10 +0200

    added basic folder structure, html, css
    Added Core folder and the following components:
    - header
    - home
    - footer
    - error

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FooterComponent } from './core/footer/footer.component';
+import { HeaderComponent } from "./core/header/header.component";
+import { HomeComponent } from "./core/home/home.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FooterComponent, HeaderComponent, HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { ErrorComponent } from './core/error/error.component';
+import { HomeComponent } from './core/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: '/home', pathMatch: 'full' },
+    { path: 'home', component: HomeComponent },
+
+    { path: '404', component: ErrorComponent },
+    { path: '**', redirectTo: '/404' },
+];
