8e4414c48c62cd99b2128bdb5a54aa64c80f583e
Plamen Borachev <plamen.borachev@gmail.com>
2024-12-11 10:49:16 +0200

    small ui changes

--- rest-api/src/config/constants.js
+++ rest-api/src/config/constants.js
@@ -2,7 +2,7 @@ export const PORT = 3000;
 
 export const DB_CONNECTION_STRING = 'mongodb://localhost:27017';
 
-export const DB_NAME = 'video-games-trade-platform'; //TODO: Modify DB name & project name in package.json and package-lock.json
+export const DB_NAME = 'video-games-trade-platform';
 
 export const JWT_SECRET = '98132hfsdiasdfvgweuhf874o2q3hfb420f9udias08u1213';
 
